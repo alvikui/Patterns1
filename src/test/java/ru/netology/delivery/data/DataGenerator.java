3b19c22cdd215dfb71540ea40eeeea8996ecea4d
Alena Kuimova <alvikui@yandex.ru>
2023-10-02 17:48:25 +0300

    Change test

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -11,36 +11,29 @@ import java.util.Random;
 public class DataGenerator {
     private DataGenerator() {
     }
-
-    public static String generateDate(int shift) {
-        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public static String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
     public static String generateCity() {
-        var cities = new String[]{"Москва", "Новосибирск", "Казань", "Омск", "Калининград", "Санкт-Петербург",
-                "Киров", "Псков", "Краснодар", "Красноярск"};
+        var cities = new String[]{"Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала",
+                "Магас", "Нальчик", "Элиста", "Черкесск", "Петрозаводск", "Сыктывкар"};
         return cities[new Random().nextInt(cities.length)];
     }
-
     public static String generateName(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.name().lastName() + " " + faker.name().firstName();
     }
-
     public static String generatePhone(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
-
     public static class Registration {
         private Registration() {
         }
-
-        public static UserInfo generateUser(String locale) {
+        public static UserInfo generateUser(String locale){
             return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
     }
-
     @Value
     public static class UserInfo {
         String city;
